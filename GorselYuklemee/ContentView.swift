5b9e5cc875715f395e153c33a491b48d9a4d1830
omer <sanli.omer@metu.edu.tr>
2022-05-25 19:31:37 +0300

    Firebase Storage Görsel Yükleme SwiftUI

--- GorselYuklemee/ContentView.swift
+++ GorselYuklemee/ContentView.swift
@@ -6,11 +6,63 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseStorage
 
 struct ContentView: View {
+    @State var butonaTiklandi: Bool = false
+    @State var image: UIImage?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button {
+                butonaTiklandi.toggle()
+            } label: {
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .scaledToFill()
+                }else{
+                    Image(systemName: "person.circle")
+                        .font(.system(size: 100))
+                }
+                
+                
+                
+            }
+            
+            Button {
+                if let image = image{
+                    let data = image.jpegData(compressionQuality: 0.5)
+                    let reference = Storage.storage().reference().child("\(UUID().uuidString).jpeg")
+                    reference.putData(data!) { metadata, error in
+                        if error == nil {
+                            print("Başarılı bir şekilde Firestore Storage'a görseliniz yüklendi")
+                            
+                            reference.downloadURL { url, error in
+                                print("Yükleme adresiniz \(url!)")
+                            }
+                            
+                        }
+                    }
+                }
+    
+                
+                
+                
+            } label: {
+                Text("Fotoğrafı Firebase Storage'a aktar")
+                    .font(.system(size: 25))
+                    .padding()
+            }
+
+            
+
+        }
+        .fullScreenCover(isPresented: $butonaTiklandi) {
+            ImagePicker(image: $image)
+        }
     }
 }
 
