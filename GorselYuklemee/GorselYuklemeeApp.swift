6e114df133b684987da7c8a323e0d1c52e65575a
omer <sanli.omer@metu.edu.tr>
2022-05-25 18:15:17 +0300

    Initial Commit

---
+++ GorselYuklemee/GorselYuklemeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GorselYuklemeeApp.swift
+//  GorselYuklemee
+//
+//  Created by omer sanli on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GorselYuklemeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
